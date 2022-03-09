89ee9ddc3bb00a49cc3432035f4687509f420a4e
angelsf <multibucketheadland@gmail.com>
2022-03-09 18:02:06 -0400

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,6 +8,12 @@ const totalLikes = (blogs) => {
   return likes.reduce((a, b) => a + b, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map(b => b.likes)
+  const maxBlogLikes = Math.max(...likes)
+  return (maxBlogLikes > 0) ? maxBlogLikes : 0
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
