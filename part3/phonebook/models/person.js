54e420b862be8c830175e3bc5a7b46f769ace6b4
angelsf <multibucketheadland@gmail.com>
2022-11-20 19:48:05 -0400

    Corregida la propiedad message al aparecer un error de conexion con mongoose

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -7,7 +7,7 @@ console.log('connecting to', url)
 
 mongoose.connect(url)
     .then(result => console.log('connected to MongoDB'))
-    .catch(error => console.log('error connecting to MongoDB: ', error.messsage))
+    .catch(error => console.log('error connecting to MongoDB: ', error.message))
 
 const personSchema = new mongoose.Schema({
     name: {
